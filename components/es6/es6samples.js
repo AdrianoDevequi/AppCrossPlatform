8ad0902d916278ebaa14e3741ff0211600a40020
AdrianoDevequi <adrianodevequi@gmail.com>
2018-01-16 19:22:17 -0200

    Commit exemplos

--- components/es6/es6samples.js
+++ components/es6/es6samples.js
@@ -100,7 +100,19 @@ function Pessoa (nome, idade){
 var lala = new Pessoa('Adriano', '21.9');
 
 
-
-Teste
 */
 
+let arr = [1,2,3,4,5,6,7,8,9,10];
+
+let arrMaisUm = arr.map((ele, index, arr) => ele + 1)
+
+let arrImpares = arrMaisUm.filter((ele, index, arr) => ele %2 == 1)
+
+let somaDosImpares = arrImpares.reduce((acc, ele, index, arr) => acc += ele, 0 ) 
+undefined
+arrMaisUm
+(10) [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+arrImpares
+(5) [3, 5, 7, 9, 11]
+somaDosImpares
+35
