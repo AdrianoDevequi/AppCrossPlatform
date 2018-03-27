d256af6947f09b4ae25edb7546f94aa6ee8b9ab0
adrianodevequi <adrianodevequi@gmail.com>
2018-03-27 14:58:38 -0300

    Redux Multiplicação

--- components/redux/app.js
+++ components/redux/app.js
@@ -49,6 +49,25 @@
 				}
 			}
 
+			function multiplicacaoReducer(state, action){
+				if(!state)
+					return new EstadoPadrao(); 
+				switch(action.type){
+				// case "@@redux/INIT":
+				// 	document.querySelector("body[unresolved]").removeAttribute("unresolved");
+				// 	return new EstadoPadrao();
+				case "MULTIPLICAR_VALORES":
+					return Object.assign(state, {
+						valor1: action.data.valor1,
+						valor2: action.data.valor2,
+						resultado: action.data.valor1 * action.data.valor2
+					});
+				case "LIMPAR_VALORES":
+					return new EstadoPadrao();
+				default:
+					return state;
+				}
+			}
 			
 			$ngReduxProvider.createStoreWith({somaState: somaReducer, subtracaoState: subtracaoReducer});
 
